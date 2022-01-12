bdb648c621968eec800aac179246d9daaa8fe668
Renan Cavalcante <66544573+renan-cav@users.noreply.github.com>
2022-01-12 14:55:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ service_account = gspread.service_account_from_dict(credentials)
 spreadsheet = service_account.open_by_key(spreadsheet_id)
 worksheet = spreadsheet.worksheet("matriculas_zn_2020")
 
+dados = pd.DataFrame(worksheet.get_all_records())
+
 app = Flask(__name__)
 @app.route("/")
 def inicio():
   return "<p>Dados Matrículas SME 2020</p>"
+          dados
