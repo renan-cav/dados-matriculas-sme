ed90163c82e5d33c211c45d101cd92260bcbfc6d
Renan Cavalcante <66544573+renan-cav@users.noreply.github.com>
2022-01-13 17:51:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-  return "<p> Página inicial</p>"
+  return dados.to_html()
 
 @app.route("/sobre")
 def sobre():
-  return "<p> Desenvolvido por Renan Cavalcante Eugenio como projeto para o curso de pós-graduação de jornalismo de dados, automação e data storytelling do Instituto Insper</p>"
-
-@app.route("/dados")
-def dados():
-  return dados.to_html()
+  return "<p> Desenvolvido por Renan Cavalcante Eugenio como projeto para o curso de pós-graduação de jornalismo de dados, automação e data storytelling do Instituto Insper</p
